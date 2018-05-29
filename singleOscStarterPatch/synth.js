4458cb7ca58ea55c3371f97a39f71b57e7ec4171
Benjamin Kleeman <bkleeman@mail.depaul.edu>
2018-05-29 15:50:25 -0500

    basic functions added for note on and note off messages, messages assigned to keyup and keydown events

--- singleOscStarterPatch/synth.js
+++ singleOscStarterPatch/synth.js
@@ -1,27 +1,61 @@
+// Declare oscillator and gainNode globally so that they can be disconnected
+
+var oscillator, gainNode;
+
 // Create Audio Context
 
 var context = new AudioContext(); // Create audio container
 
-// Oscillator
+function noteOn(frequency) {
+  
+  // Oscillator, Waveform Attributes
 
-oscillator = context.createOscillator(); // Create oscillator 
+  oscillator = context.createOscillator(); // Create oscillator 
+  oscillator.type = "sine"; // Set Oscillator waveform
+  oscillator.frequency.value = frequency; // Frequency in Hz is set by the function parameter 
+  oscillator.start(0); // start oscillator, parameter is delay time in seconds
+  
+  // Gain Node
 
-// Gain Node
+  gainNode = context.createGain(); // Create gain node
+  gainNode.gain.value = 1; // Adjust volume 
+  
+  // Signal Routing
 
-gainNode = context.createGain(); // Create gain node
+  oscillator.connect(gainNode); // Connect oscillator to gain node 
+  gainNode.connect(context.destination); // Connect gain node to speakers
+  
+}; // end noteOn()
 
-// Waveform Attributes
+function noteOff() {
+  
+  oscillator.stop(0); // Stop the oscillator, parameter is delay time in seconds
+  oscillator.disconnect(); // call to the garbage collector so you can call noteOn() again
+  
+}; // end noteOff()
 
-oscillator.type = "sine"; // Set Oscillator waveform
-oscillator.frequency.value = 200; // Set frequency in hz
+$(document).keydown(function(e) {
+  console.log(e);
+  switch (e.keyCode) {
+    case 90: // Z
+      noteOn(82.407);
+    break;
+  }
+}); // end keydown event.
 
-// Signal Routing
+$(document).keyup(function(e) {
+  console.log(e);
+  switch (e.keyCode) {
+    case 90: // Z
+      noteOff();
+    break;
+  }
+}); // end keydown event.
 
-oscillator.connect(gainNode); // Connect oscillator to gain node 
-gainNode.connect(context.destination); // Connect gain node to speakers
 
-// Volume Adjustments
 
-gainNode.gain.value = 0.5; // Adjust volume via gain node
 
-oscillator.start(0); // start oscillator
+
+
+
+
